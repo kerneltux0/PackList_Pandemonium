962586a3377ad1ac7b1539278224a8ec55bc3f43
Ryan <kerneltux0@gmail.com>
2019-07-01 10:10:27 -0600

    setup main app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "go9odl2uckgu5ess8ingthi5so1neha0cke7rs"
   end
 
   get "/" do
