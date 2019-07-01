567c2fce0aad9accdcf57b3e13dc31cd0f995f34
Ryan <kerneltux0@gmail.com>
2019-07-01 10:11:33 -0600

    setup list controller

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+
+  get '/lists/new' do
+    erb :'/lists/new'
+
+  end
+
+  get '/lists/index' do
+    erb :'/lists/index'
+
+  end
+  
+  get '/lists/:id' do
+    @list = params[:id]
+    erb :'/lists/show'
+
+  end
+
+  get '/lists/:id/edit' do
+    erb :'/lists/edit'
+
+  end
+
+  get '/lists/:id/delete' do
+    erb :'/lists/delete'
+
+  end
+
+  post '/lists/new' do
+    binding.pry
+
+  end
+  
+end
