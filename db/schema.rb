47c789c9af277ac0722a42fca8c1bddc769cc764
Ryan <kerneltux0@gmail.com>
2019-07-01 13:40:06 -0600

    removed tables after changes to models

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190701163541) do
-
-  create_table "items", force: :cascade do |t|
-    t.string  "name"
-    t.integer "list_id"
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.string  "name"
-    t.string  "items"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "lists"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
