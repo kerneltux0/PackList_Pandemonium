675ecf7826752139107de8ccd93a3a0f03caf091
Ryan <kerneltux0@gmail.com>
2019-07-03 13:06:25 -0600

    added baselist model for easier list editing

---
+++ app/models/baselist.rb
@@ -0,0 +1,13 @@
+class BaseList < ApplicationController
+
+  def self.general
+    ["identification", "insurance", "tickets", "emergency_contacts", "reservation_documents"]
+
+  end
+
+  def self.camping
+
+  end
+
+  
+end
