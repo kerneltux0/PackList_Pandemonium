44ea2b7da05cbf80ca3861c37f9e2bc46ae8d108
Ryan <kerneltux0@gmail.com>
2019-07-03 13:37:24 -0600

    setup class methods returning arrays of items

--- app/models/baselist.rb
+++ app/models/baselist.rb
@@ -5,7 +5,23 @@ class BaseList < ApplicationController
 
   end
 
+  def self.electronics
+    ["mobile_phone", "charging_cables", "tablet", "laptop", "mp3_player", "e_book_reader", "extra_batteries"]
+
+  end
+
   def self.camping
+    ["compass", "map", "tent", "tent_poles", "tent_stakes", "insect_repellant", "epi_pen", "cooler", "crushed_ice", "snacks", "coffee"]
+
+  end
+
+  def self.toiletries
+    ["toothbrush", "toothpaste", "medications", "deodorant", "sunscreen", "soap", "first_aid_kit"]
+
+  end
+
+  def self.clothing
+    ["shirts", "pants", "shorts", "jacket", "hat", "sunglasses", "shoes", "sandals", "sleepwear", "dress", "laundry_bag", "belts"]
 
   end
 
