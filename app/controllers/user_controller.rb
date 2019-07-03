c130cdacc9534486f985c11d2754cb57df970394
Ryan <kerneltux0@gmail.com>
2019-07-03 14:06:33 -0600

    added step to remove lists when deleting a user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -83,6 +83,7 @@ class UserController < ApplicationController
       if params[:delete_acct] == "on"
         @user = User.find(session[:user_id])
         session.clear
+        @user.lists.destroy
         @user.destroy
         erb :'/user/delete_conf'
       else
