e581abbd2387ebc8a0739524b464ac97567b8282
Ryan <kerneltux0@gmail.com>
2019-07-08 10:49:02 -0600

    added items to baselist

--- app/models/baselist.rb
+++ app/models/baselist.rb
@@ -1,27 +1,27 @@
 class BaseList < ApplicationController
 
   def self.general
-    ["identification", "insurance", "tickets", "emergency_contacts", "reservation_documents"]
+    ["identification", "itinerary", "house_keys", "maps", "insurance", "tickets", "emergency_contacts", "reservation_documents", "cash", "notepad", "pen/pencil", "guidebook"]
 
   end
 
   def self.electronics
-    ["mobile_phone", "charging_cables", "tablet", "laptop", "mp3_player", "e_book_reader", "extra_batteries"]
+    ["mobile_phone", "charging_cables", "tablet", "laptop", "mp3_player", "e-book_reader", "extra_batteries", "memory_cards", "headphones/earbuds", "camera"]
 
   end
 
   def self.camping
-    ["compass", "map", "tent", "tent_poles", "tent_stakes", "insect_repellant", "epi_pen", "cooler", "crushed_ice", "snacks", "coffee"]
+    ["compass", "tent", "tent_poles", "tent_stakes", "insect_repellant", "cooler", "crushed_ice", "fishing_pole", "bait/lures", "fishing_license", "snacks", "drinks", "coffee", "tarps", "rope", "bungee_cords", "multi-tool_kit", "hiking_shoes"]
 
   end
 
   def self.toiletries
-    ["toothbrush", "toothpaste", "medications", "deodorant", "sunscreen", "soap", "first_aid_kit"]
+    ["toothbrush", "toothpaste", "medications", "deodorant", "sunscreen", "soap", "epi_pen", "first_aid_kit", "nail-clippers/files", "make-up", "eye_drops", "feminine_hygene_products", "skin_care_lotions", "lip_balm"]
 
   end
 
   def self.clothing
-    ["shirts", "pants", "shorts", "jacket", "hat", "sunglasses", "shoes", "sandals", "sleepwear", "dress", "laundry_bag", "belts"]
+    ["shirts", "pants", "shorts", "jacket", "hat(s)", "socks", "sunglasses", "shoes", "sandals", "sleepwear", "dresses/skirts", "laundry_bag", "belts", "sunglasses", "swimwear", "slippers"]
 
   end
 
